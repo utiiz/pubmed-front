b9993db1106f7cc7074da642925363b4be0f77f8
Tanguy Kervran <tanguy_kervran@ext.connect-tech.sncf>
2023-10-03 16:41:11 +0200

    Download CSV

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,50 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
-import axios from 'axios'
+import { Button, Input } from '@material-tailwind/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-  useEffect(() => {
-    const url = "https://wrybmteixi.execute-api.eu-west-3.amazonaws.com/pubmed";
+  const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState("");
+  const generatePubmedFile = () => {
+    setLoading(true);
+    console.log(url);
+    const urlBase = "https://wrybmteixi.execute-api.eu-west-3.amazonaws.com/pubmed";
     const options = {
       method: "POST",
-      mode: "no-cors",
+      // body: JSON.stringify({ url: "https://pubmed.ncbi.nlm.nih.gov/?term=(glucose)+and+(continuous)+and+(monitoring+or+measurement)+and+(diabetes)&filter=pubt.guideline&filter=pubt.meta-analysis&filter=pubt.systematicreview&filter=datesearch.y_10&filter=hum_ani.humans&filter=lang.english&filter=lang.french&timeline=expanded&format=pubmed&size=10&page=1"})
+      body: JSON.stringify({ url })
     };
-    fetch(url, options)
-      .then((response) => console.log);
-  }, [])
+    fetch(urlBase, options)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        let rows = [["Titre", "Auteur", "Date", "Type", "Link"]];
+        data.forEach(el => {
+          const link = "https://pubmed.ncbi.nlm.nih.gov/" + el["PMID"]
+          rows.push([
+            el["TI"],
+            el["AU"] || el["CN"],
+            el["DP"],
+            el["PT"],
+            link
+          ]);
+        });
+        const csvContent = "data:text/csv;charset=utf-8," 
+          + rows.map(e => e.join(";")).join("\n");
+        const encodedUri = encodeURI(csvContent);
+        window.open(encodedUri);
+      })
+      .finally(() => setLoading(false));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="w-96 flex flex-col space-y-4">
+          <Input label='URL' color='indigo' value={url} onChange={(event) => setUrl(event.target.value)}/>
+          <Button color='indigo' disabled={loading} fullWidth ripple={false} onClick={() => generatePubmedFile()}>Générer le fichier</Button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
